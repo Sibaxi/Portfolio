83c7868332038101e19469c1cbac058cbe5a2887
Manuel <manuel.felda@gmail.com>
2023-04-18 19:22:24 +0200

    changed content

--- components/About.tsx
+++ components/About.tsx
@@ -9,7 +9,7 @@ export default function About({ onVisible }: Props) {
     <section
       id="About"
       data-scroll-section
-      className="lg:min-h-screen flex flex-col lg:h-full justify-center lg:px-40 py-10 lg:py-40"
+      className="flex flex-col justify-center py-10 lg:min-h-screen lg:h-full lg:px-40 lg:py-40"
     >
       <InView
         threshold={0.6}
@@ -17,12 +17,12 @@ export default function About({ onVisible }: Props) {
           onVisible(inView);
         }}
       >
-        <h2 className="text-3xl lg:text-5xl text-white font-bold font-heading">
+        <h2 className="text-3xl font-bold text-white lg:text-5xl font-heading">
           About
         </h2>
-        <p className="text-white mt-8 text-sm lg:text-base max-w-xl">
-          I&apos;m a Front-End developer located in Slovenia. I studided graphic
-          design but of out a desire to build my own designs, I learned
+        <p className="max-w-xl mt-8 text-sm text-white lg:text-base">
+          I&apos;m a Front-End developer located in Slovenia. I studied graphic
+          design but out of a desire to build my own designs, I learned
           programming. I enjoy building interactive and dynamic experiences with
           nonintrusive animations and UI effects. Find out more about my work
           experience on
@@ -30,21 +30,21 @@ export default function About({ onVisible }: Props) {
             href={`https://www.linkedin.com/in/manuel-felda-563a66184/`}
             target="_blank"
             rel="noreferrer"
-            className=" ml-1 underline"
+            className="ml-1 underline "
           >
             LinkedIn
           </a>{" "}
           or check out some of my
-          <a href={`#Work`} data-scroll-to className=" ml-1 underline">
+          <a href={`#Work`} data-scroll-to className="ml-1 underline ">
             work
           </a>
           .
         </p>
 
-        <h3 className="mt-6 max-w-xl text-lg text-white font-heading border-b pb-1 border-gray-400">
+        <h3 className="max-w-xl pb-1 mt-6 text-lg text-white border-b border-gray-400 font-heading">
           Stack
         </h3>
-        <p className="mt-4 text-white text-sm lg:text-base">
+        <p className="mt-4 text-sm text-white lg:text-base">
           React, Next, Vue, Nuxt, Tailwind CSS
         </p>
       </InView>
